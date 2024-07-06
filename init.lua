fac47cb777908e703811d9d460175d41dd4d0e77
Lance Campbell <lkcampbell@gmail.com>
2024-07-06 10:20:13 -0400

    Add keymap for nvim-tree toggle

--- init.lua
+++ init.lua
@@ -174,6 +174,11 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Oil keymaps
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Go to Oil-based parent directory of file buffer' })
 
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Nvim-Tree keymaps
+vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { desc = 'Toggle [N]vim-Tree view on and off', noremap = true, silent = true })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
